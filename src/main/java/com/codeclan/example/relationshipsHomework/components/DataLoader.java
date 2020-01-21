a02776f30977e6030f8a91e181a73305fcfad600
Catrin <catrinhughes92@gmail.com>
2020-01-21 16:23:21 +0000

    mvp done

--- src/main/java/com/codeclan/example/relationshipsHomework/components/DataLoader.java
+++ src/main/java/com/codeclan/example/relationshipsHomework/components/DataLoader.java
@@ -1,5 +1,8 @@
 package com.codeclan.example.relationshipsHomework.components;
 
+import com.codeclan.example.relationshipsHomework.models.File;
+import com.codeclan.example.relationshipsHomework.models.Folder;
+import com.codeclan.example.relationshipsHomework.models.User;
 import com.codeclan.example.relationshipsHomework.repositories.FileRepository;
 import com.codeclan.example.relationshipsHomework.repositories.FolderRepository;
 import com.codeclan.example.relationshipsHomework.repositories.UserRepository;
@@ -26,5 +29,16 @@ public class DataLoader implements ApplicationRunner {
 
     public void run(ApplicationArguments args) {
 
+        User user1 = new User("Max");
+        userRepository.save(user1);
+
+        Folder folder1 = new Folder("Mad Max", user1);
+        folderRepository.save(folder1);
+
+        File file1 = new File("Thunderdome", "ppt", 30, folder1);
+        fileRepository.save(file1);
+
+        File file2 = new File("Fury Road", "ppt", 25, folder1);
+        fileRepository.save(file2);
     }
 }
