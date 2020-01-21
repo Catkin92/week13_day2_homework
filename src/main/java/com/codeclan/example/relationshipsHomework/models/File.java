ecb0b4586b1309f00235a53c185612f509c76067
Catrin <catrinhughes92@gmail.com>
2020-01-21 15:47:08 +0000

    created many to one relationship in file

--- src/main/java/com/codeclan/example/relationshipsHomework/models/File.java
+++ src/main/java/com/codeclan/example/relationshipsHomework/models/File.java
@@ -1,5 +1,7 @@
 package com.codeclan.example.relationshipsHomework.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
@@ -19,14 +21,20 @@ public class File {
     @Column(name = "size")
     private int size;
 
+    @JsonIgnoreProperties("files")
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    private Folder folder;
+
     public File() {
 
     }
 
-    public File(String name, String extension, int size) {
+    public File(String name, String extension, int size, Folder folder) {
         this.name = name;
         this.extension = extension;
         this.size = size;
+        this.folder = folder;
     }
 
     public long getId() {
@@ -59,5 +67,13 @@ public class File {
 
     public void setSize(int size) {
         this.size = size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
     }
 }
