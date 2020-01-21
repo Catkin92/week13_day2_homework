2400e58de6bf3c66340e26307d69bde665d9ac23
Catrin <catrinhughes92@gmail.com>
2020-01-21 15:32:39 +0000

    created class File

---
+++ src/main/java/com/codeclan/example/relationshipsHomework/RelationshipsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.relationshipsHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipsHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/relationshipsHomework/RelationshipsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.relationshipsHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
