c13b24524677337e44f75ce2cd16ee8fbd9d6827
Catrin <catrinhughes92@gmail.com>
2020-01-21 15:34:59 +0000

    created user class

---
+++ src/main/java/com/codeclan/example/relationshipsHomework/User.java
@@ -0,0 +1,39 @@
+package com.codeclan.example.relationshipsHomework;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public User() {
+
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
